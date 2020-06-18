6f8ee2c06613065351807303a5b8c2b4dffbf353
Steve Mooney <smooney27@gmail.com>
2020-06-18 11:24:24 -0700

    Update ACMT Test Code

--- TestDockerACMT.R
+++ TestDockerACMT.R
@@ -1,11 +1,14 @@
-
+getwd()
+setwd("C:\\Users\\sjm2186\\Downloads\\docker2\\docker-acmt\\")
 source("GeocoderACMT.R")
-location <- geocode("1959 NE Pacific Street, Seattle, WA 98195")
+
+# Test Geocoder
+location <- geocode("1959 NE Pacific Street, Seattle, WY 98195")
+
+# Test ACMT
+measures <- get_acmt_standard_array(-122.333, 47.663, 2000, year=2010)  
+
+# Test Both
+location <- geocode("1959 NE Pacific Street, Seattle, WY 98195")
 measures <- get_acmt_standard_array(location$longitude, location$latitude, 2000, year=2010)  
-location[1]
-
-
-location <- geocode("515 W Olympic Pl, Seattle, WA 98195")
-measures <- get_acmt_standard_array(location$longitude, location$latitude, 2000, year=2010)  
-
 data.frame(measures$names, round(measures$values, 2))
